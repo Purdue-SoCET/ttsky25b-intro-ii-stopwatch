2a06d92f023f74324d1c3cc2502841a099bf22ed
Miguel Isrrael Teran <misrrael@purdue.edu>
2025-10-21 15:56:10 -0400

    Fill out YAML file, project top-level source file, and config.json

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_intro_ii_stopwatch (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,22 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[7:4] = '0;  // Unused outputs
+  assign uio_oe = '1;  // Will use ALL bidirectional pins as outputs
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[7:4], uio_in, 1'b0};
+
+  // Instantiate project
+  intro_2_stopwatch stopwatch_proj (
+    .clk(clk),
+    .n_rst(rst_n),
+    .BTN(ui_in[3:0]),
+    .D0_AN_0(uo_out[0]),
+    .D0_AN_1(uo_out[1]),
+    .D0_AN_2(uo_out[2]),
+    .D0_AN_3(uo_out[3]),
+    .D0_SEG(uio_out)
+  );
 
 endmodule
